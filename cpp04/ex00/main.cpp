c93a3e3b27f9fea68bbed395b2f1dc91df986b87
ecaruso42 <eugenio.caruso99@gmail.com>
2024-07-22 17:24:01 +0200

    Wrong implementato

--- cpp04/ex00/main.cpp
+++ cpp04/ex00/main.cpp
@@ -1,6 +1,8 @@
 #include "Animal.hpp"
 #include "Cat.hpp"
 #include "Dog.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
 
 int main()
 {
@@ -10,12 +12,18 @@ int main()
 
 	std::cout << j->getType() << " " << std::endl;
 	std::cout << i->getType() << " " << std::endl;
-	i->makeSound(); //will output the cat sound!
+	i->makeSound();
 	j->makeSound();
 	meta->makeSound();
 	delete j;
 	delete i;
 	delete meta;
+	const WrongAnimal* WrongA = new WrongAnimal();
+	const WrongCat* WrongC = new WrongCat();
+	WrongC->makeSound();
+	WrongA->makeSound();
+	delete WrongC;
+	delete WrongA;
 
 	return 0;
 }
