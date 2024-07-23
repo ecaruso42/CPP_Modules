357e6d8ebd9b08b394d81fcf75b32a974a226e4e
ecaruso42 <eugenio.caruso99@gmail.com>
2024-07-23 18:45:06 +0200

    da testare il copy constructor

--- cpp04/ex01/Cat.cpp
+++ cpp04/ex01/Cat.cpp
@@ -2,6 +2,7 @@
 
 Cat::Cat(){
 	this->_type = "Cat";
+	this->_brain = new Brain();
 	std::cout << "Default constructor for " << this->_type << " called" << std::endl;
 }
 
@@ -14,12 +15,15 @@ Cat::Cat(const Cat& other) : Animal(other){
 Cat& Cat::operator = (const Cat& other){
 	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &other){
+		delete this->_brain;
 		this->_type = other._type;
+		this->_brain = new Brain(*other._brain);
 	}
 	return *this;
 }
 
 Cat::~Cat(){
+	delete this->_brain;
 	std::cout << "Destructor for " << this->_type << " called" << std::endl;
 }
 
--- cpp04/ex01/Dog.cpp
+++ cpp04/ex01/Dog.cpp
@@ -2,6 +2,7 @@
 
 Dog::Dog(){
 	this->_type = "Dog";
+	this->_brain = new Brain();
 	std::cout << "Default constructor for " << this->_type << " called" << std::endl;
 }
 
@@ -14,12 +15,15 @@ Dog::Dog(const Dog& other) : Animal(other){
 Dog& Dog::operator = (const Dog& other){
 	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &other){
+		delete this->_brain;
 		this->_type = other._type;
+		this->_brain = new Brain(*other._brain);
 	}
 	return *this;
 }
 
 Dog::~Dog(){
+	delete this->_brain;
 	std::cout << "Destructor for " << this->_type << " called" << std::endl;
 }
 
